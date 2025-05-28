6153dbebaa8dd580f30846e5574e3dfd3d27b2c8
IsmetKonan <thecodasemler@gmail.com>
2025-05-28 20:44:10 +0200

    Add files via upload

---
+++ SS2752025/java/java.js
@@ -0,0 +1,12 @@
+//Small Framework by I.K
+
+// Variables 
+const dataoflogs = document.getElementById('dataoflogs')
+const input = document.getElementById('cmds')
+let starttext = "C:// "
+
+// edeting Variable
+dataoflogs.replaceWith( starttext + 'This Text gets Displayed trough JavaScript')
+
+// Control for Input
+input.onclick(alert('lol'))
